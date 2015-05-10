a4b771c862c168c9056254d5cad959a6a41ae009
Jonathan Baillie Strong <Jonathan Baillie Strong>
2015-05-10 11:44:27 +0100

    Added blog section

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,41 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+var colors = ['72,35,68', '43,81,102', '66,152,103', '250,178,67', '224,33,48'];
+var RSSUrl = encodeURIComponent('http://jonbstrong.com/blog/feed');
+var $feedTitle = $('#header-title');
+var $posts = $('#posts');
+var $btnHori = $('#btn-hori');
+var $btnVert = $('#btn-vert');
+
+// get the posts
+$.ajax({
+  url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=2.0&q='+RSSUrl+'&num=3',
+  dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
+  success: createPosts
+});
+
+function createPosts(json) {
+  $feedTitle.html(json.responseData.feed.title);
+  var posts = json.responseData.feed.entries;
+  var j = 0;
+  
+  // for each post in the feed create and append the HTML
+  for(var i = 0; i < posts.length; i++) {
+    var $post = $(document.createElement('div'));
+    var $bg = $(document.createElement('div'));
+    var $content = $(document.createElement('div'));
+    $bg.css({background: 'rgba('+colors[j]+',1)'});
+    $bg.addClass('bg');
+    $bg.html('<a class="post-link" target="_blank" href="'+posts[i].link+'"></a>');
+    $content.html('<h3>'+posts[i].title+'</h3>'+ '<p class="more">read more</p>');
+    $content.addClass('content');
+    
+    $post.append($content);
+    $post.append($bg);
+    $post.addClass('post');
+    $posts.append($post);
+    j = j == 4 ? 0 : j+1;
+  }   
+}
